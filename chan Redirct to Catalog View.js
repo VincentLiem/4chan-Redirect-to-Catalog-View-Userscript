41e22f1f415c69bd9ab69f7f2722a1c1a3dbeb7e
petkeeper18 <96896686+petkeeper18@users.noreply.github.com>
2022-02-02 18:28:59 -0800

    Create chan Redirct to Catalog View.js

---
+++ chan Redirct to Catalog View.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         4chan Redirct to Catalog View
+// @version      1.0
+// @match        *boards.4chan.org/*/
+// @match        *boards.4channel.org/*/
+// @run-at       document-start
+
+// ==/UserScript==
+
+window.location.replace(window.location.href + "catalog");
