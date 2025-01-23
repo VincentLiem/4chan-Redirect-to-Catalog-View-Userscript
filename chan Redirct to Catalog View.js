dafa142cfdfc2664d4b7e84df16f728d83050a2c
Vincent Liem <96896686+VincentLiem@users.noreply.github.com>
2025-01-23 15:43:16 -0800

    Update chan Redirct to Catalog View.js
    exclude flash board

--- chan Redirct to Catalog View.js
+++ chan Redirct to Catalog View.js
@@ -3,6 +3,7 @@
 // @version      1.0
 // @match        *boards.4chan.org/*/
 // @match        *boards.4channel.org/*/
+// @exclude      *boards.4chan.org/f/
 // @run-at       document-start
 
 // ==/UserScript==
